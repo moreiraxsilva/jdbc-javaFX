985037efe53b1ef0d9383a51e810156e68d6a923
moreiraxsilva <danniloxmoreira@gmail.com>
2020-04-28 00:17:29 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -59,8 +62,9 @@ public class DepartmentFormController implements Initializable {
 			Utils.currentStage(event);
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving objet", null, e.getMessage(), AlertType.ERROR);
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErros());
 		}
-		
 	}
 	
 	private void notifyDataChangeListeners() {
@@ -71,9 +75,17 @@ public class DepartmentFormController implements Initializable {
 
 	private Department getFormData() {
 		Department obj = new Department();
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(textId.getText()));
+		if(textName.getText() == null || textName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty.");
+		}
 		obj.setName(textName.getText());
 		
+		if (exception.getErros().size() > 0) {
+			throw exception;
+		}
 		return obj;
 	}
 
@@ -99,9 +111,18 @@ public class DepartmentFormController implements Initializable {
 		dataChangeListeners.add(listener);
 	}
 	
+	private void setErrorMessages(Map<String,String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")){
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		inicializeNodes();
 	}
+	
 
 }
