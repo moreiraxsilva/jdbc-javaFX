24b6f9d453b72c266890acd269963d634ce908c6
moreiraxsilva <danniloxmoreira@gmail.com>
2020-04-29 22:22:01 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -107,10 +109,30 @@ public class SellerFormController implements Initializable {
 		ValidationException exception = new ValidationException("Validation error");
 		
 		obj.setId(Utils.tryParseToInt(textId.getText()));
+		
 		if(textName.getText() == null || textName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty.");
 		}
 		obj.setName(textName.getText());
+		
+		if(textEmail.getText() == null || textEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty.");
+		}
+		obj.setEmail(textEmail.getText());
+		
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("dpBirthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+		if(textBaseSalary.getText() == null || textBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty.");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(textBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
 		
 		if (exception.getErros().size() > 0) {
 			throw exception;
@@ -156,9 +178,11 @@ public class SellerFormController implements Initializable {
 	private void setErrorMessages(Map<String,String> errors) {
 		Set<String> fields = errors.keySet();
 		
-		if (fields.contains("name")){
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBithDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
+	
 	}
 	
 	public void loadAssociatedObjects() {
